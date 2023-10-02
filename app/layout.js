3eba0e6893f39247e50760a669d9311c48958a2b
parksydev <t50048298@gmail.com>
2023-10-02 14:26:00 +0900

    layout.js 수정

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,12 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: '옥소폴리틱스 정치성향테스트',
   description: '옥소폴리틱스 정치성향 테스트 by.옥소',
-  viewport: 'initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width',
+  viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
 }
 
 export default function RootLayout({ children }) {
