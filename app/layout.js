2beb9eda6403c2037a9dc863f953d1bfe1c90ac2
parksydev <t50048298@gmail.com>
2023-10-02 22:50:04 +0900

    Head title 수정

--- app/layout.js
+++ app/layout.js
@@ -17,13 +17,15 @@ export const metadata = {
       },
     ],
   },
-  title: '옥소폴리틱스 정치성향 테스트',
   viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
 }
 
 export default function RootLayout({ children }) {
   return (  
     <html lang="ko">
+      <Head>
+        <title>옥소폴리틱스 정치성향 테스트</title>
+      </Head>
       <body className={inter.className} style={{margin: "0", padding: "0"}}>{children}</body>
     </html>
   )
