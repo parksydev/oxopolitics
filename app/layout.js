4c42a79bd09844b00952d3ae21ec31e1ae5dd17d
parksydev <t50048298@gmail.com>
2023-10-02 21:10:16 +0900

    opengraph 수정

--- app/layout.js
+++ app/layout.js
@@ -5,17 +5,24 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: '옥소폴리틱스 정치성향테스트',
-  description: '옥소폴리틱스 정치성향 테스트 by.옥소',
+  openGraph: {
+    title: '옥소폴리틱스 정치성향테스트',
+    description: '옥소폴리틱스 정치성향 테스트 by.옥소',
+    url: 'https://oxopolitics.kr',
+    sitename: 'oxopolitics',
+    type: 'website',
+    images: [
+      {
+        url: 'https://oxopolitics.com/assets/assets/og_image_mid.jpg',
+      },
+    ],
+  },
   viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
 }
 
 export default function RootLayout({ children }) {
   return (  
     <html lang="ko">
-      <Head>
-        <meta property="og:image" content="https://oxopolitics.com/assets/assets/og_image_mid.jpg" />
-      </Head>
       <body className={inter.className} style={{margin: "0", padding: "0"}}>{children}</body>
     </html>
   )
