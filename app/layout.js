ff12c6258ce3c6ed155bef074ceef14f7e7e33b9
parksydev <t50048298@gmail.com>
2023-10-02 14:16:23 +0900

    metadata 수정

--- app/layout.js
+++ app/layout.js
@@ -7,15 +7,12 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: '옥소폴리틱스 정치성향테스트',
   description: '옥소폴리틱스 정치성향 테스트 by.옥소',
+  viewport: 'initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-    <Head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" />
-    </Head>
       <body className={inter.className} style={{margin: "0", padding: "0"}}>{children}</body>
     </html>
   )
